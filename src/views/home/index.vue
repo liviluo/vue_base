43cd2a14becaf6ed125183718cae2a5ad44ff2f6
Livi W Luo <livi.w.luo@pccw.com>
2022-01-17 15:09:01 +0800

    修改：目录结构

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <SayHello msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import SayHello from '@/components/home/say-hello.vue'
+export default {
+  components: {
+    SayHello
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
